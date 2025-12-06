3ec056a9f1bf82684a5bc978269f334f238ca4a4
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-06 17:11:18 +0000

    Add simple input box

--- web/src/pages/home/Home.tsx
+++ web/src/pages/home/Home.tsx
@@ -1,3 +1,19 @@
+import { useState, type ChangeEvent } from "react";
+
 export const HomePage = () => {
-  return "Hello, world";
+  const [text, setText] = useState("");
+
+  async function changeText(e: ChangeEvent<HTMLInputElement>) {
+    setText(e.target.value);
+  }
+
+  return (
+    <form>
+      <label>
+        Enter some text:
+        <input type="text" value={text} onChange={changeText} />
+      </label>
+      <p>Return: {text}</p>
+    </form>
+  );
 };
