77c3a7acf6a5fa2986ad33105d30ba01ff65bb10
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-06 18:34:20 +0000

    Integrate with server

--- web/src/pages/home/Home.tsx
+++ web/src/pages/home/Home.tsx
@@ -1,10 +1,20 @@
 import { useState, type ChangeEvent } from "react";
 import { useDebounce } from "../../hooks/useDebounce";
+import { trpc } from "../../servs/client";
 
 export const HomePage = () => {
   const [text, setText] = useState("");
 
   const debouncedText = useDebounce(text, 500);
+
+  const {data, isLoading} = trpc.hello.useQuery(
+    { name: debouncedText },
+
+    {
+      enabled: debouncedText.length > 0,
+      refetchInterval: 2000,
+    }
+  );
 
   async function changeText(e: ChangeEvent<HTMLInputElement>) {
     setText(e.target.value);
@@ -18,6 +28,7 @@ export const HomePage = () => {
       </label>
       <p>Text: {text}</p>
       <p>Debounced: {debouncedText}</p>
+      <p>Resp: {isLoading ? "Loading..." : data}</p>
     </form>
   );
 };
