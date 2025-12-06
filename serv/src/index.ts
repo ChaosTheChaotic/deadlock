99c761df23d33627d72007b14ea949a011739895
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-06 17:57:57 +0000

    Make server serve files

--- serv/src/index.ts
+++ serv/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
+import path from "path";
 
 const app = express();
-const port = 8888;
+const port = process.env.PORT || 8888;
+
+const cdp = path.join(__dirname, "../../web/dist");
+app.use(express.static(cdp))
 
 app.use(
   "/trpc",
@@ -12,6 +16,11 @@ app.use(
   }),
 );
 
+app.get(/^\/(?!trpc).*/, (req, res) => {
+  res.sendFile(path.join(cdp, 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+  console.log(`Frontend served from: ${cdp}`);
 });
