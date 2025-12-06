1c0ff24149d6dfb2c7ef5114824b34c54ccc5cae
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-06 20:59:44 +0000

    Update run to build napi

--- tools/run.sh
+++ tools/run.sh
@@ -11,6 +11,13 @@ PR="$(dirname -- "$ABP")"
 cd $PR/web
 pnpm build && echo "Sucessfully built the web" || fatal "Failed to build web"
 
+CRATES="${PR%/}/serv/src/crates"
+
+for dir in "$CRATES"/*/; do
+  dirn="${dir%/}"
+  (cd "$dirn" && pnpm build && echo "Built $dirn" || echo "Failed to build $dirn")
+done
+
 cd $PR/serv
 pnpm build && echo "Successfully built server" || fatal "Failed to build server"
 pnpm start:prod
