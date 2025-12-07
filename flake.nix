2c5727002c1d3d75e505f24497de955adfce8240
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-07 00:18:07 +0000

    Start working on database

--- flake.nix
+++ flake.nix
@@ -20,7 +20,18 @@
             prettier
 	    eslint
 	    napi-rs-cli
+	    docker-compose
+	    postgresql
+	    cargo
           ];
+          shellHook = ''
+            echo "To start PostgreSQL: docker-compose up -d"
+            echo "To stop PostgreSQL: docker-compose down"
+            echo "To connect via CLI: psql -h localhost -U postgres -d postgres"
+            echo "To connect to UIDB: psql -h localhost -U postgres -d UIDB"
+            echo "To connect to GRIDS: psql -h localhost -U postgres -d GRIDS"
+            echo "With app user: psql -h localhost -U app_user -d UIDB"
+          '';
         };
       });
 }
