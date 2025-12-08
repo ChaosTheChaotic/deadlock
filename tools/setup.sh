a9ae74c30ba24aa06bac0896a8337b825fa07cba
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-08 14:01:01 +0000

    Finish setup script

--- tools/setup.sh
+++ tools/setup.sh
@@ -7,3 +7,10 @@ source $ABP/utils.sh
 check_installed "pnpm"
 check_installed "cargo"
 check_installed "rustc"
+
+cd $ABP/../web && pnpm i || fatal "Failed to install dependencies for website"
+echo "Installed deps for the website successfully"
+
+cd $ABP/../serv && pnpm i || fatal "Failed to install dependencies for the server"
+cd $ABP/../serv/src/crates/db && pnpm i || fatal "Failed to install dependencies for database rust module"
+echo "Successfully installed all deps for server"
