b69eaa236a49c988ad7c78cc053ddf7a2120e28e
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-11 15:58:17 +0000

    Try to improve setup script

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ABP=$(dirname $(realpath "$0"))
+PR="$(dirname -- "$ABP")"
 
 source $ABP/utils.sh
 
@@ -10,5 +11,16 @@ cd $ABP/../web && pnpm i || fatal "Failed to install dependencies for website"
 echo "Installed deps for the website successfully"
 
 cd $ABP/../serv && pnpm i || fatal "Failed to install dependencies for the server"
-cd $ABP/../serv/src/crates/db && pnpm i || fatal "Failed to install dependencies for database rust module"
+
+CRATES="${PR%/}/serv/src/crates"
+
+for dir in "$CRATES"/*/; do
+  dirn="${dir%/}"
+  (cd "$dirn" && pnpm i || fatal "Failed to install deps for rust module: $dirn")
+done
+
 echo "Successfully installed all deps for server"
+
+echo "Setup complete!"
+echo "Note that although the main setup is complete, a postgresql server is needed"
+echo "You may do this via docker (using the docker-compose.yml provided) or via an actual installation"
