7e2a7e0234cdf4d34c235f6e9b31d4acd97ce1fd
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-12 08:34:32 +0000

    Add frotnend input for adding a new user

--- web/src/pages/home/Home.tsx
+++ web/src/pages/home/Home.tsx
@@ -8,6 +8,10 @@ export const HomePage = () => {
   const [dbSearch, setDBSearch] = useState("");
   const [isInitialized, setIsInitialized] = useState(false);
   const [initError, setInitError] = useState<string | null>(null);
+  const [newUserEmail, setNewUserEmail] = useState("");
+  const [newUserPassword, setNewUserPassword] = useState("");
+  const [newUserDisplay] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const debouncedText = useDebounce(text, 500);
   const debouncedDB = useDebounce(dbSearch, 500);
@@ -61,6 +65,14 @@ export const HomePage = () => {
     e: ChangeEvent<HTMLInputElement>,
   ) {
     callback(e.target.value);
+  }
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  async function addNewUser(email: string, pass: string) {
+    // TODO: Add linking to the server endpoint for adding a new user
   }
 
   return (
@@ -117,6 +129,23 @@ export const HomePage = () => {
       <p>
         Users: {isUsersLoading ? "Loading..." : JSON.stringify(users, null, 2)}
       </p>
+      <input
+	type="text"
+	value={newUserEmail}
+        onChange={(e) => changeText(setNewUserEmail, e)}
+      />
+      <input
+	type={showPassword ? "text" : "password"}
+	value={newUserPassword}
+	onChange={(e) => changeText(setNewUserPassword, e)}
+      />
+      <button type="button" onClick={togglePasswordVisibility}>
+        {showPassword ? 'Hide' : 'Show'} Password
+      </button>
+      <button type="button" onClick={() => addNewUser(newUserEmail, newUserPassword)}>
+	Submit user info
+      </button>
+      <p>New User: {newUserDisplay}</p>
     </>
   );
 };
