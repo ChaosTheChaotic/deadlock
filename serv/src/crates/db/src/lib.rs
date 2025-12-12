8db62b244b9b836b4809fea46ae7aabd2196c16d
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-12 17:06:40 +0000

    Run formatter

--- serv/src/crates/db/src/lib.rs
+++ serv/src/crates/db/src/lib.rs
@@ -1,12 +1,11 @@
+use argon2::{
+    Argon2,
+    password_hash::{PasswordHasher, SaltString},
+};
 use deadpool_postgres::{ManagerConfig, Pool, RecyclingMethod};
 use napi_derive::napi;
 use tokio::sync::OnceCell;
 use tokio_postgres::{NoTls, Row};
-use argon2::{
-    Argon2, password_hash::{
-        PasswordHasher, SaltString
-    }
-};
 
 #[napi(object)]
 pub struct User {
@@ -156,12 +155,12 @@ pub async fn add_user(
     if pass.is_none() && oauth_provider.is_none() {
         return Err(napi::Error::from_status(napi::Status::InvalidArg));
     }
-    
+
     let client = get_uidb_pool()
         .get()
         .await
         .map_err(|e| napi::Error::from_reason(format!("Failed to get client from pool: {e}")))?;
-    
+
     if let Some(password) = pass {
         // Hash the password with Argon2
         let salt = SaltString::generate(&mut rand_core::OsRng);
@@ -170,7 +169,7 @@ pub async fn add_user(
             .hash_password(password.as_bytes(), &salt)
             .map_err(|e| napi::Error::from_reason(format!("Failed to hash password: {e}")))?
             .to_string();
-        
+
         // Prepare and execute insert statement for password-based user
         let stmt = client
             .prepare_cached(
@@ -184,18 +183,18 @@ pub async fn add_user(
             )
             .await
             .map_err(|e| napi::Error::from_reason(format!("Failed to prepare cached: {e}")))?;
-        
+
         let row = client
             .query_one(&stmt, &[&email, &password_hash])
             .await
             .map_err(|e| {
                 napi::Error::from_reason(format!("Failed to execute insert statement: {e}"))
             })?;
-        
+
         Ok(user_from_row(row))
     } else {
         let provider = oauth_provider.as_ref().unwrap();
-        
+
         let stmt = client
             .prepare_cached(
                 "INSERT INTO users (email, oauth_provider) VALUES ($1, $2) 
@@ -208,14 +207,14 @@ pub async fn add_user(
             )
             .await
             .map_err(|e| napi::Error::from_reason(format!("Failed to prepare cached: {e}")))?;
-        
+
         let row = client
             .query_one(&stmt, &[&email, provider])
             .await
             .map_err(|e| {
                 napi::Error::from_reason(format!("Failed to execute insert statement: {e}"))
             })?;
-        
+
         Ok(user_from_row(row))
     }
 }
