3c898383b6d6c5ecb731d42b87a0e84860132b02
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-17 21:34:46 +0000

    Update fix.sh

--- tools/fix.sh
+++ tools/fix.sh
@@ -4,7 +4,8 @@ ABP=$(dirname $(realpath "$0"))
 
 source $ABP/utils.sh
 
-check_installed "pnpm"
+check_common_deps
+check_installed "rustfmt"
 
 PR="$(dirname -- "$ABP")"
 
@@ -13,3 +14,6 @@ pnpm fix && echo "Fixed web" || fatal "Failed to fix web"
 
 cd $PR/serv
 pnpm fix && echo "Fixed server" || fatal "Failed to fix server"
+
+cd $PR/serv/src/crates
+cargo clippy --fix && echo "Fixed Rust" || fatal "Failed to fix rust"
